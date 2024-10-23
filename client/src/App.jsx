056636de92cb4cc0702cde1455b9b8252970653c
Pietro Gabriel <pietro.menezes@aluno.senai.br>
2024-10-23 15:49:17 -0300

    Tcc pietro

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Atualizar from './components/pages/Atualizar';
-import Bem_vindo_gestao from './components/pages/Bem_Vindo_Gestao';
+import Bem_Vindo_Gestao from './components/pages/Bem_Vindo_Gestao';
 import Controle_Usuarios from './components/pages/Controle_Usuarios';
 import Financeiro from './components/pages/Financeiro';
-import Login from './components/pages/Login_Gestao';
 import Cadastro from './components/pages/Cadastro';
 import EditCadastro from './components/pages/EditCadastro';
-<<<<<<< HEAD
-
-
-=======
-<<<<<<< HEAD
-import LoginUsuario from './components/pages/LoginUsuario';
+import Login_Usuario from './components/pages/Login_Usuario';
 import Tela_profissionais from './components/pages/Tela_profissionais';
-import Perfil_profissional from './components/pages/Perfil_profissional';
 
 
 
-=======
-import Perfil_Profissional from './components/pages/Perfil_Profissional';
->>>>>>> 3766b76105c29e24fdef3dfa7e835e72f47d04da
->>>>>>> 184a0403aace9e6610d2122564edc8733540da8e
+
 
 function App() {
   return (
         <Router>
           <Routes>
-            <Route path='/Bem_vindo_gestao' element={<Bem_vindo_gestao/>} /> 
+            <Route path='/Bem_vindo' element={<Bem_Vindo_Gestao/>} /> 
             <Route path='/Atualizar' element={<Atualizar/>} /> 
             <Route path='/Controle' element={<Controle_Usuarios/>}/>
             <Route path='/Financeiro' element={<Financeiro/>}/>
-            <Route path='/Login' element={<Login/>}/>
+            <Route path='/Login' element={<Login_Usuario/>}/>
             <Route path='/cadastro' element={<Cadastro/>}/>
             <Route path='/EditCadastro/:id' element={<EditCadastro/>}/>
-<<<<<<< HEAD
-            <Route path='/LoginUsuario' element={<LoginUsuario/>}/>
+            <Route path='/LoginUsuario' element={<Login_Usuario/>}/>
             <Route path='/tela_profissionais' element={<Tela_profissionais/>}/>
-=======
-            <Route path='/Perfil_Profissional' element={<Perfil_Profissional/>}/>
->>>>>>> 3766b76105c29e24fdef3dfa7e835e72f47d04da
+           
+
           
           </Routes>
         </Router> 
