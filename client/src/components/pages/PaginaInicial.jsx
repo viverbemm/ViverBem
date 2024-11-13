24a95f7e5d6871389c625c2292a6f9ef7f14c73c
Pietro Gabriel <pietro.menezes@aluno.senai.br>
2024-11-13 15:51:18 -0300

    tcc

--- client/src/components/pages/PaginaInicial.jsx
+++ client/src/components/pages/PaginaInicial.jsx
@@ -14,22 +14,13 @@ function PaginaInicial() {
                             {`Nosso site é uma plataforma de fácil navegação, onde você pode encontrar profissionais qualificados`} <br />
                             {`para ajudar seu ente querido no dia a dia, garantindo um atendimento humanizado e seguro.`} <br /><br />
                             {`Para os cuidadores, oferecemos um catálogo virtual que amplia a visibilidade de seus serviços.`} <br />
-                            {`Por uma taxa acessível, de R$ 29,90 mensais ou R$ 299,90 anuais, os cuidadores têm seus contatos`} <br />
+                            {`Por uma taxa acessível, de `} 
+                            <strong className={styles.valorMensal}>R$ 29,90 mensais</strong> {` ou `} <strong className={styles.valorAnual}>R$ 299,90 anuais</strong>, {` os cuidadores têm seus contatos`} <br />
                             {`exibidos no site, conectando-se a famílias que buscam assistência especializada.`} <br /><br />
-
                             {`Aqui, valorizamos o cuidado e a confiança. Explore nosso site e encontre o suporte ideal para quem você ama,`} <br />
                             {`ou cadastre-se como cuidador e expanda seu alcance profissional.`}
                         </p>
-
-
                     </section>
-                </div>
-
-                <div className={styles.containerMensal}>
-                    <h1 className={styles.planosMensais}><b className={styles.planoMensal}>Plano mensal</b></h1>
-                    <p className={styles.mensal}>
-                        {`R$29,90`}<br/>
-                    </p>
                 </div>
             </main>
         </div>
