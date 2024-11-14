8e6305e34d44d5be8b791c201c472e159a7ea368
JOÃO PEDRO COSTA SILVA <j.silva@labcetec.net>
2024-11-14 17:26:22 -0300

    14/11

--- client/src/components/pages/Perfil_profissional.jsx
+++ client/src/components/pages/Perfil_profissional.jsx
@@ -53,11 +53,15 @@ function Profissional() {
         <div className={stylesP.containerI}>
           <img src={profilePic} alt="Luísa Heringer" className={stylesP.profilePic} />
           <div className={stylesP.introText}>
-            <h2>Olá, eu sou a Luísa Heringer.</h2>
+            <h2>ﾠﾠﾠﾠﾠOlá, eu sou a Luísa Heringer.</h2>
+            <p>ﾠ</p>
             <p className={stylesP.introText_p}>
-              Meu nome é Luísa Heringer, e tenho o privilégio de dedicar minha vida ao cuidado dos idosos há 3 anos e 6 meses.
-              Minha paixão por esta profissão é impulsionada pela empatia e respeito pelos idosos, e meu objetivo é proporcionar
-              uma qualidade de vida excepcional para aqueles que confiam em meus cuidados.
+              <p className={stylesP.pI}>Meu nome é Luísa Heringer, e tenho o privilégio de</p>
+              <p className={stylesP.pII}>dedicar minha vida ao cuidado dos idosos há 3 anos e 6</p>
+              <p className={stylesP.pIII}>meses. Minha paixão por esta profissão é impulsionada</p>
+              <p className={stylesP.pIV}>pela empatia e respeito pelos idosos, e meu objetivo é</p>
+              <p className={stylesP.pV}>proporcionar uma qualidade de vida excepcional para</p>
+              <p className={stylesP.pVI}>aqueles que confiam em meus cuidados.</p>
             </p>
           </div>
         </div>
@@ -67,13 +71,14 @@ function Profissional() {
         <div className={stylesP.containerII}>
           <div className={stylesP.desenvol}>
             <strong><h3>Sobre mim:</h3></strong>
-            <p>Experiência Profissional: Cuidadora de Idosos</p>
-            <p>Cuidadora autônoma para idoso com doença de Parkinson avançada (2020-2023)</p>
-            <p>
-              Durante 3 anos e 6 meses, atuei como cuidadora autônoma dedicada a um senhor idoso diagnosticado com
-              doença de Parkinson avançada. Minhas responsabilidades incluíam:
+            <p>ﾠ</p>
+            <p className={stylesP.pI}>Experiência Profissional: Cuidadora de Idosos</p>
+            <p className={stylesP.pII}>Cuidadora autônoma para idoso com doença de Parkinson avançada (2020-2023)</p>
+            <p className={stylesP.pIII}>Durante 3 anos e 6 meses, atuei como cuidadora autônoma dedicada a um senhor idoso
+              <p>diagnosticado com doença de Parkinson avançada. Minhas responsabilidades incluíam:</p>
             </p>
             <ul>
+              <p>ﾠ</p>
               <li>Assistência diária</li>
               <li>Gerenciamento de sintomas</li>
               <li>Estimulação cognitiva</li>
@@ -81,10 +86,10 @@ function Profissional() {
               <li>Suporte emocional</li>
               <li>Coordenação com profissionais de saúde</li>
             </ul>
-            <p>
-              Essa experiência não apenas aprimorou minhas habilidades práticas, mas também reforçou minha compreensão
-              da importância do cuidado holístico e individualizado para idosos que enfrentam condições de saúde complexas.
-            </p>
+            <p>ﾠ</p>
+            <p className={stylesP.pIII}>Essa experiência não apenas aprimorou minhas habilidades práticas, mas também reforçou</p>
+            <p className={stylesP.pIII}>minha compreensão da importância do cuidado holístico e individualizado para idosos que</p>
+            <p className={stylesP.pIII}>enfrentam condições de saúde complexas.</p>
           </div>
         </div>
       </section>
@@ -97,6 +102,7 @@ function Profissional() {
           <div className={stylesP.reservar}>
             <p className={stylesP.text}>Estou sempre em busca de novas</p>
             <p className={stylesP.text}>oportunidades. Entre em contato.</p>
+            <p>ﾠ</p>
             <a href="#" className={stylesP.button}>Reservar</a>
           </div>
         </div>
@@ -105,7 +111,8 @@ function Profissional() {
       <section className={stylesP.rod}>
         <div className={stylesP.containerIV}>
           <h1 className={stylesP.rodape}>ViverBem+</h1>
-          <p className={stylesP.controd}>Centro Empresarial Shopping Praia da Costa, 245, Vila Velha, ES</p>
+          <p className={stylesP.pI}>Centro Empresarial Shopping</p>
+          <p className={stylesP.pII}>Praia da Costa, 245, Vila Velha, ES</p>
         </div>
       </section>
     </div>
