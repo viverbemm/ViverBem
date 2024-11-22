944d912f00a4e00ce1a5aff73f6a964035225d58
isaac <isaac.barbara@aluno.senai.br>
2024-11-22 17:24:29 -0300

    SERVER, BANCO DE DADOS, CONEXAO

--- isaac/js/script.js
+++
@@ -1,7 +0,0 @@
-// Seleciona o elemento com o ID 'open_btn', que é o botão para abrir/fechar a barra lateral
-document.getElementById('open_btn').addEventListener('click', function () {
-
-    // Seleciona o elemento com o ID 'sidebar', que é a barra lateral
-    // Em seguida, alterna a classe 'open-sidebar' para abrir ou fechar a barra lateral
-    document.getElementById('sidebar').classList.toggle('open-sidebar');
-});
