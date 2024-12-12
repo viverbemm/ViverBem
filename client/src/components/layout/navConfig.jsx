f8f056ebeefc33ced7799bccbceebf857d257529
Joao Pedro <joao.p.silva94@aluno.senai.br>
2024-12-12 09:02:06 -0300

    tudo dará certo 2.0 :)

--- client/src/components/layout/navConfig.jsx
+++ client/src/components/layout/navConfig.jsx
@@ -1,27 +1,37 @@
 import logo from '../pages/imagens/logox.png';
 import styles from './navConfig.module.css';
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; 
 
+function NavConfig() {
+    const navigate = useNavigate(); 
 
-function navConfig() {
+    const handleLogout = () => {
+        setTimeout(() => {
+            navigate('/'); 
+        }, 2000);
+    };
 
-    return(
+    return (
         <div className={styles.navPrinc}>
             <header>
                 <div className={styles.navBarDirei}>
-                    <a href="/"><img src={logo} alt="" style={{height: '80px', with: '80px', marginLeft: '120px' }} /></a>
-
+                    <a href="/">
+                        <img
+                            src={logo}
+                            alt="Logo"
+                            style={{ height: '80px', width: '200px', marginLeft: '120px' }}
+                        />
+                    </a>
                 </div>
                 <div className={styles.navBarEsqu}>
                     <a href="/perfil">Minha Conta</a>
                     <a href="/Config">Configurações</a>
-                    <a href="/PaginaInicial">Sair</a>
+                    <a href="#" onClick={handleLogout}>Sair</a>
                 </div>
             </header>
-
         </div>
-    )
-
+    );
 }
-export default navConfig;
 
+export default NavConfig;
